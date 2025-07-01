b2c6c1239dc5ed7ef402de9ce838c1dfd5ac6e9f
Abdul Wadood <156080188+Abd-Wadood@users.noreply.github.com>
2025-07-01 20:26:46 +0500

    updated Database

--- pos_system.sql
+++ pos_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2025 at 11:48 AM
+-- Generation Time: Jul 01, 2025 at 05:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,7 +38,10 @@ CREATE TABLE `bills` (
 --
 
 INSERT INTO `bills` (`bill_id`, `total_amount`, `created_at`) VALUES
-(11, 570.00, '2025-06-30 09:44:25');
+(44, 650.00, '2025-06-30 19:29:04'),
+(45, 1600.00, '2025-06-30 19:29:33'),
+(46, 4590.00, '2025-06-30 19:30:29'),
+(47, 2020.00, '2025-07-01 14:52:10');
 
 -- --------------------------------------------------------
 
@@ -60,13 +63,14 @@ CREATE TABLE `brands` (
 --
 
 INSERT INTO `brands` (`id`, `name`, `price`, `expiration_date`, `quantity`, `category_id`) VALUES
-(4, 'coke 1.5L', 90.00, '2025-05-05', 100, 15),
-(5, 'coke 0.5', 50.00, '2025-05-05', 99, 15),
-(6, 'zinger', 250.00, '2025-05-05', 100, 18),
-(7, 'large pizza', 1500.00, '2025-05-05', 20, 20),
-(8, 'small pizza', 520.00, '2025-05-05', 29, 20),
-(9, 'medium pizza', 970.00, '2052-02-05', 25, 20),
-(10, 'grill roll', 490.00, '2025-05-05', 50, 21);
+(13, 'coke 1.5L', 100.00, '2025-05-05', 48, 24),
+(14, 'large pizza', 1500.00, '2025-05-05', 21, 28),
+(15, 'grill roll', 490.00, '2025-05-05', 50, 27),
+(16, 'zinger', 520.00, '2025-05-05', 48, 26),
+(17, 'coke 0.5', 50.00, '2025-05-05', 99, 24),
+(18, 'small pizza', 600.00, '2025-05-05', 19, 28),
+(19, 'grill burger', 500.00, '2025-05-05', 50, 26),
+(20, 'medium pizza', 970.00, '2025-05-05', 24, 28);
 
 -- --------------------------------------------------------
 
@@ -84,11 +88,173 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
-(15, 'drinks'),
-(16, 'breads'),
-(18, 'chicken'),
-(20, 'pizza dough'),
-(21, 'Paratha');
+(24, 'drinks'),
+(25, 'breads'),
+(26, 'chicken'),
+(27, 'Paratha'),
+(28, 'pizza dough');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `phone`, `address`) VALUES
+(16, 'wadood', '355326', 'Askari-1 37-f Lahore Cantt'),
+(17, 'abdul ', '03264926955', 'Lahore Cantt'),
+(18, 'muaz', '3553265451', 'Askari-1 '),
+(19, 'abdul wadood', '355326', 'Askari-1 37-f Lahore Cantt');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_details`
+--
+
+CREATE TABLE `customer_details` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `bill_id` int(11) NOT NULL,
+  `brand_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer_details`
+--
+
+INSERT INTO `customer_details` (`id`, `customer_id`, `bill_id`, `brand_name`) VALUES
+(15, 16, 44, 'Multiple'),
+(16, 17, 45, 'Multiple'),
+(17, 18, 46, 'Multiple'),
+(18, 19, 47, 'Multiple');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_details`
+--
+
+CREATE TABLE `payment_details` (
+  `id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `bill_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment_details`
+--
+
+INSERT INTO `payment_details` (`id`, `payment_id`, `customer_id`, `bill_id`) VALUES
+(9, 15, 16, 44),
+(10, 16, 17, 45),
+(11, 17, 18, 46),
+(12, 18, 19, 47);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_methods`
+--
+
+CREATE TABLE `payment_methods` (
+  `id` int(11) NOT NULL,
+  `payment_type` varchar(50) NOT NULL,
+  `payment_time` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment_methods`
+--
+
+INSERT INTO `payment_methods` (`id`, `payment_type`, `payment_time`) VALUES
+(15, 'Cash', '2025-07-01 00:29:04'),
+(16, 'Bank Transfer', '2025-07-01 00:29:33'),
+(17, 'Card', '2025-07-01 00:30:29'),
+(18, 'Bank Transfer', '2025-07-01 19:52:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+
+CREATE TABLE `purchases` (
+  `id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price_per_unit` decimal(10,2) NOT NULL,
+  `purchase_date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `purchases`
+--
+
+INSERT INTO `purchases` (`id`, `supplier_id`, `quantity`, `price_per_unit`, `purchase_date`) VALUES
+(1, 15, 25, 500.00, '2025-07-01 19:05:50'),
+(2, 13, 2, 1000.00, '2025-07-01 19:10:10'),
+(4, 13, 50, 450.00, '2025-07-01 19:53:10'),
+(5, 13, 25, 25.00, '2025-07-01 20:01:12'),
+(6, 13, 25, 25.00, '2025-07-01 20:12:28'),
+(7, 13, 25, 25.00, '2025-07-01 20:13:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_details`
+--
+
+CREATE TABLE `purchase_details` (
+  `id` int(11) NOT NULL,
+  `purchase_id` int(11) NOT NULL,
+  `detail_description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `purchase_details`
+--
+
+INSERT INTO `purchase_details` (`id`, `purchase_id`, `detail_description`) VALUES
+(1, 1, 'kilograms'),
+(2, 2, 'kilograms'),
+(3, 7, 'kgs');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_sessions`
+--
+
+CREATE TABLE `sales_sessions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `start_time` datetime DEFAULT current_timestamp(),
+  `total_sale` decimal(10,2) DEFAULT 0.00,
+  `logout_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales_sessions`
+--
+
+INSERT INTO `sales_sessions` (`id`, `user_id`, `start_time`, `total_sale`, `logout_time`) VALUES
+(11, 1, '2025-07-01 00:21:39', 6840.00, '2025-07-01 00:30:56'),
+(12, 1, '2025-07-01 00:34:41', 0.00, NULL),
+(13, 1, '2025-07-01 18:49:19', 2020.00, '2025-07-01 19:53:34'),
+(14, 1, '2025-07-01 19:54:21', 0.00, NULL);
 
 -- --------------------------------------------------------
 
@@ -108,11 +274,11 @@ CREATE TABLE `suppliers` (
 --
 
 INSERT INTO `suppliers` (`id`, `name`, `phone`, `category_id`) VALUES
-(5, 'anwar', '03004606068', 15),
-(6, 'waqas', '03004606055', 21),
-(7, 'waqas', '03004606055', 16),
-(8, 'naeem', '03264926922', 18),
-(9, 'kitchen', '04236632426', 20);
+(13, 'anwar', '03004606068', 24),
+(14, 'waqas', '03004606055', 25),
+(15, 'naeem', '04236632426', 28),
+(16, 'waqas', '03004606055', 27),
+(17, 'kitchen', '03264926922', 26);
 
 -- --------------------------------------------------------
 
@@ -157,6 +323,56 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer_details`
+--
+ALTER TABLE `customer_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `bill_id` (`bill_id`);
+
+--
+-- Indexes for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `payment_id` (`payment_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `bill_id` (`bill_id`);
+
+--
+-- Indexes for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `purchase_id` (`purchase_id`);
+
+--
+-- Indexes for table `sales_sessions`
+--
+ALTER TABLE `sales_sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `suppliers`
 --
 ALTER TABLE `suppliers`
@@ -177,25 +393,67 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bills`
 --
 ALTER TABLE `bills`
-  MODIFY `bill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `bill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `customer_details`
+--
+ALTER TABLE `customer_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `sales_sessions`
+--
+ALTER TABLE `sales_sessions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -212,6 +470,39 @@ ALTER TABLE `users`
 --
 ALTER TABLE `brands`
   ADD CONSTRAINT `brands_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `customer_details`
+--
+ALTER TABLE `customer_details`
+  ADD CONSTRAINT `customer_details_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `customer_details_ibfk_2` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`bill_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `payment_details`
+--
+ALTER TABLE `payment_details`
+  ADD CONSTRAINT `payment_details_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payment_methods` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `payment_details_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `payment_details_ibfk_3` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`bill_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD CONSTRAINT `purchases_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `purchase_details`
+--
+ALTER TABLE `purchase_details`
+  ADD CONSTRAINT `purchase_details_ibfk_1` FOREIGN KEY (`purchase_id`) REFERENCES `purchases` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `sales_sessions`
+--
+ALTER TABLE `sales_sessions`
+  ADD CONSTRAINT `sales_sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `suppliers`
